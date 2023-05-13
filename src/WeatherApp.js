f1bfe2c1a0e6ca17a72b19a0be367c59f7e4960d
Vlad <hrinyov@gmail.com>
2023-05-14 01:38:54 +0300

    connect data to html

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const WeatherApp = () => {
-    const [city, setCity] = useState('');
+    const [city, setCity] = useState('Uzhorod');
     const [latitude, setLatitude] = useState('');
     const [longitude, setLongitude] = useState('');
     const [weatherData, setWeatherData] = useState(null);
@@ -11,10 +11,9 @@ const WeatherApp = () => {
         setCity(event.target.value);
     };
 
-    useEffect(() => {
         const fetchWeatherData = async () => {
             try {
-                const apiKey = process.env.REACT_APP_API_KEY;
+                const apiKey = 'f368f0713d293c43f48f399695bb6c7e';
 
                 const geoCodeUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
                 const geoCodeResponse = await axios.get(geoCodeUrl);
@@ -24,9 +23,9 @@ const WeatherApp = () => {
                     setLatitude(lat);
                     setLongitude(lon);
 
-                    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+                    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
                     const weatherResponse = await axios.get(weatherUrl);
-
+                    console.log(weatherData);
                     setWeatherData(weatherResponse.data);
                 } else {
                     // Handle case when no coordinates are found for the entered city
@@ -39,57 +38,45 @@ const WeatherApp = () => {
             }
         };
 
-        if (city) {
-            fetchWeatherData();
-        }
-    }, [city]);
-
     return (
         <div>
+            <div className='input'>
+                <input
+                type="text"
+                value={city}
+                onChange={handleInputChange}
+                placeholder="Enter city name"
+                />
+                <button onClick={fetchWeatherData}>Search</button>    
+            </div>
             <div className='container'>
             <div className='top'>
                 <div className='location'>
-                    <p>London</p>
+                    <p>{weatherData?.name}</p>
                 </div>
                 <div className='temp'>
-                    <h1>12°C</h1>
+                    <h1>{weatherData?.main.temp}°C</h1>
                 </div>
                 <div className='description'>
-                    <p>Clouds</p>
+                    <p>{weatherData?.weather[0].main}</p>
                 </div>
             </div>
 
             <div className='bottom'>
                 <div className='feels'>
-                    <p>12°C</p>
+                    <p className='bold'>{weatherData?.main.feels_like}°C</p>
+                    <p>Feels like</p>
                 </div>
                 <div className='humidity'>
-                    <p>20%</p>
+                    <p className='bold'>{weatherData?.main.humidity}%</p>
+                    <p>Humidity</p>
                 </div>
                 <div className='wind'>
-                    <p>2 kmph</p>
+                    <p className='bold'>{weatherData?.wind.speed} km/h</p>
+                    <p>Wind speed</p>
                 </div>
             </div>
             </div>
-            {/* <h1>Weather App</h1>
-            <input
-                type="text"
-                value={city}
-                onChange={handleInputChange}
-                placeholder="Enter city name"
-            />
-            {latitude && longitude && (
-                <div>
-                    <h2>Coordinates: {latitude}, {longitude}</h2>
-                </div>
-            )}
-            {weatherData && (
-                <div>
-                    <h2>{weatherData.name}</h2>
-                    <p>Temperature: {weatherData.main.temp}°C</p>
-                    <p>Weather: {weatherData.weather[0].main}</p>
-                </div>
-            )} */}
         </div>
     );
 };
